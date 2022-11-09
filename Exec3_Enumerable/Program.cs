54803f152e4ff83db31ecf4d1fd33925b68f54aa
w2192 <w2192@LAPTOP-QEVPLLMQ>
2022-11-09 23:49:51 +0800

    c

--- Exec3_Enumerable/Program.cs
+++ Exec3_Enumerable/Program.cs
@@ -10,6 +10,16 @@ namespace Exec3_Enumerable
 	{
 		static void Main(string[] args)
 		{
+			IEnumerable<int> list1 = Enumerable.Range(1, 10);
+			foreach (int i in list1)
+			{
+				Console.WriteLine(i);
+			}
+			IEnumerable<int> list2 = Enumerable.Range(10, 6);
+			foreach (int i in list2)
+			{
+				Console.WriteLine(i);
+			}
 		}
 	}
 }
